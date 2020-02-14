5f90fde7997d4988025e7e1aa326f90323da4ec7
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-02-14 22:09:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ class App < Sinatra::Base
 
   # Add your post route and action below
   
-  post '/food' do
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+post '/food' do
+    params.to_s
   end
-
+  
 end
